4492167a2016ac8fa1b2653fdb227d657c62fcd6
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2023-05-22 20:50:55 +0900

    Add files via upload

---
+++ TextRPG/TextRPG/TextRPG.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void EnterLobby();
+void SelectPlayer();
+
+int main()
+{
+
+
+
+}
+
+void EnterLobby()
+{
+	while (true)
+	{
+		cout << "--------------------------------" << endl;
+		cout << " 로비에 입장했습니다! " << endl;
+		cout << "--------------------------------" << endl;
+		
+		//플레이어 직업 선택
+
+	
+	}
+	
+	
+
+}
+
+void SelectPlayer()
+{
+	cout << "--------------------------------" << endl;
+	cout << "  직업을 골라주세요! " << endl;
+	cout << "--------------------------------" << endl;
+}
